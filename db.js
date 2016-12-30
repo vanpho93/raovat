be1053ffd4c1b601bf354062f775baf1dcf519f3
Admin <vanpho02@gmail.com>
2016-12-30 14:17:07 +0700

    xong db

--- db.js
+++ db.js
@@ -1,32 +1,9 @@
 var pg = require('pg');
 
 //For Heroku
-// var URI = 'postgres://ysodwyxoimuuyu:cc607f76e72c0ef8e071b84977fb635c71a0646632c77c83634176ae2324bb60@ec2-54-225-127-147.compute-1.amazonaws.com:5432/d89pev1q0kgsra';
-// function query(sql, cb){
-//   pg.connect(URI, (err, client, done) => {
-//     if(err) return cb(err);
-//     done();
-//     client.query(sql, (err, result) => {
-//       if(err) return cb(err);
-//       return cb(err, result);
-//     });
-//   });
-// }
-
-//For localhost
-var config = {
-  user: 'postgres',
-  password: 'khoapham',
-  host: 'localhost',
-  port: 5432,
-  database: 'RaoVatSaiGon',
-  idleTimeoutMillis: 500,
-  max: 100
-}
-var pool = new pg.Pool(config);
-
+var URI = 'postgres://ysodwyxoimuuyu:cc607f76e72c0ef8e071b84977fb635c71a0646632c77c83634176ae2324bb60@ec2-54-225-127-147.compute-1.amazonaws.com:5432/d89pev1q0kgsra';
 function query(sql, cb){
-  pool.connect((err, client, done) => {
+  pg.connect(URI, (err, client, done) => {
     if(err) return cb(err);
     done();
     client.query(sql, (err, result) => {
@@ -35,6 +12,29 @@ function query(sql, cb){
     });
   });
 }
+
+//For localhost
+// var config = {
+//   user: 'postgres',
+//   password: 'khoapham',
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'RaoVatSaiGon',
+//   idleTimeoutMillis: 500,
+//   max: 100
+// }
+// var pool = new pg.Pool(config);
+//
+// function query(sql, cb){
+//   pool.connect((err, client, done) => {
+//     if(err) return cb(err);
+//     done();
+//     client.query(sql, (err, result) => {
+//       if(err) return cb(err);
+//       return cb(err, result);
+//     });
+//   });
+// }
 
 function insertDB(title, desc, name, phone, image, price, address, idDistrict, idTieuMuc, cb){
   var sql = `INSERT INTO public."RaoVat"(
