f72d7306d3caba0993e9ae87103383c0402f9379
Admin <vanpho02@gmail.com>
2016-12-30 13:56:40 +0700

    dix db

--- db.js
+++ db.js
@@ -1,17 +1,7 @@
 var pg = require('pg');
-var config = {
-  user: 'postgres',
-  password: 'khoapham',
-  host: 'localhost',
-  port: 5432,
-  database: 'RaoVatSaiGon',
-  idleTimeoutMillis: 500,
-  max: 100
-}
-var pool = new pg.Pool(config);
-
+var URI = 'postgres://ysodwyxoimuuyu:cc607f76e72c0ef8e071b84977fb635c71a0646632c77c83634176ae2324bb60@ec2-54-225-127-147.compute-1.amazonaws.com:5432/d89pev1q0kgsra';
 function query(sql, cb){
-  pool.connect((err, client, done) => {
+  pg.connect(URI, (err, client, done) => {
     if(err) return cb(err);
     done();
     client.query(sql, (err, result) => {
@@ -25,6 +15,7 @@ function insertDB(title, desc, name, phone, image, price, address, idDistrict, i
   var sql = `INSERT INTO public."RaoVat"(
 	title, description, name, phone, image, address, "idQuan", price, "idTieuMuc")
 	VALUES ('${title}', '${desc}', '${name}', '${phone}', '${image}', '${address}', ${idTieuMuc}, ${price}, ${idTieuMuc});`
+  //console.log(sql);
   query(sql, (err, result) => {
     console.log(String(err));
   })
@@ -51,4 +42,4 @@ function getProductByTieuMuc(id, cb){
 function getProductSearch(text, cb){
   query(`SELECT * FROM "RaoVat" WHERE lower("title") LIKE '%${text}%'`, cb)
 }
-module.exports = {getListProduct, getProduct, insertDB, getCategory, getProductByTieuMuc, getProductSearch};
+module.exports = {query, getListProduct, getProduct, insertDB, getCategory, getProductByTieuMuc, getProductSearch};
